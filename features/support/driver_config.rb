950ebb423bc43a0e6606190e09092f4d31af9092
gkhakare <ganesh.e.khakare@gmail.com>
2015-10-15 18:01:08 +0530

    Update driver_config.rb

--- features/support/driver_config.rb
+++ features/support/driver_config.rb
@@ -45,6 +45,16 @@ def launch_driver_opera
   @driver.manage.timeouts.page_load = 120
 end
 
+ def launch_driver_android_chrome
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    client.timeout = 120
+    caps = {'chromeOptions'=> {'androidPackage' => 'com.android.chrome'}}
+    @driver = Selenium::WebDriver.for(:remote, :http_client => client, :url => "http://localhost:9515", :desired_capabilities => caps)
+    @driver.manage.timeouts.implicit_wait = 60
+
+  end
+
+
 #Android browser
 def launch_driver_android
   @driver = Selenium::WebDriver.for :remote, :desired_capabilities => :android
