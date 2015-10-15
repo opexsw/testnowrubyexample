ef2182aa859e296a49653ff4dbfcab5d70d49fb8
gkhakare <ganesh.e.khakare@gmail.com>
2015-10-15 18:19:03 +0530

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -12,7 +12,7 @@ Before do
       launch_driver_android
     when "opera"
       launch_driver_opera
-    when "AndroidChrome"
+    when "androidchrome"
       launch_driver_android_chrome  
     when "ie"
       launch_driver_ie
