22b86c7d8a550c36428fe4538f639507fd0a506d
Kaushal Rupani <krupani@users.noreply.github.com>
2015-12-15 11:52:12 +0530

    [TestNowRuby-17]- Reformat code

--- features/support/htmlformatter.rb
+++ features/support/htmlformatter.rb
@@ -1,20 +1,20 @@
-#features/support/htmlformatter.rb
 require 'cucumber/formatter/html'
+
 module Formatter 
-class HtmlFormatter < Cucumber::Formatter::Html
-  def embed(src, mime_type, label)
-  case(mime_type)
-  when /^image\/(png|gif|jpg|jpeg)/
-  super(src, mime_type, label)
-  #embed_image(src, label)
-  when /^text\/html/
-  embed_link(src, label)
-   end
- end
-  def embed_link(src, label)
-  @builder.span(:class => 'embed') do |pre| 
-pre << %{<a href="#{src}" target="_blank">"#{label}"</a> } 
-end
-end 
-end
+  class HtmlFormatter < Cucumber::Formatter::Html
+    def embed(src, mime_type, label)
+      case(mime_type)
+        when /^image\/(png|gif|jpg|jpeg)/
+          super(src, mime_type, label)  
+        when /^text\/html/
+          embed_link(src, label)
+      end
+    end
+  
+    def embed_link(src, label)
+      @builder.span(:class => 'embed') do |pre| 
+        pre << %{<a href="#{src}" target="_blank">"#{label}"</a> } 
+      end
+    end 
+  end
 end
